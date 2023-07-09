19f31fd3ec1c509e0cbd68711c30f98339a9ca63
vivi-appknot <vivi@appknot.com>
2023-07-09 14:53:14 +0900

    [CNG] 로딩바 작업중

--- js/main.js
+++ js/main.js
@@ -21,7 +21,9 @@ let count = 0;
 imgArr.forEach(img => {
     img.onload = () => {
         count++;
-
+        const percent = parseInt((count / 200) * 100);
+        mask.querySelector('p').innerHTML = percent + '%';
+        mask.querySelector('.bar').style.width = percent + '%';
         if(count == num) {
             console.log('이미지 소스 로딩 완료');
             mask.classList.add('off');
